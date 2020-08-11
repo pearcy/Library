9e6ab92db28a700eab5d0bdec5d8c796be7a4e1d
Joseph Pearce <joseph.pearce05@gmail.com>
2020-08-11 11:32:02 -0700

    add Create Action -- BooksController
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -20,9 +20,22 @@ namespace Library.Controllers
     {
       return View(_db.Books.ToList());
     }
-
-    
-
+    public ActionResult Create()
+    {
+      ViewBag.AuthorId = new SelectList(_db.Authors, "AuthorId", "AuthorName");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Book book, int AuthorId)
+    {
+      _db.Books.Add(book);
+      if (AuthorId != 0)
+      {
+        _db.AuthorBook.Add(new AuthorBook() { AuthorId = AuthorId, BookId = book.BookId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
 
